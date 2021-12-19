4c262b51e5afd886056bb8917bb8bd58ce754320
devenpich <sotheareaktuon@gmail.com>
2021-12-19 12:20:03 +0700

    this is first commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transaction;
+
+  TransactionList(this.transaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: transaction.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "No transaction added yet!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/image/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView(
+              children: transaction.map((tx) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).primaryColor, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ${tx.amount.toStringAsFixed(2)}', //Using String Interpolation
+
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tx.title,
+                            style: Theme.of(context).textTheme.headline6),
+                        Text(
+                          DateFormat.yMMMMd().format(tx.date),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            ),
+    );
+  }
+}
